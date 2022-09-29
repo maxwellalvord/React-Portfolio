13effbd67246df44c3c101cdd8358989d68af9f0
Maxwell Alvord <maxwellalvord@gmail.com>
2022-09-29 14:12:45 -0700

    add completed nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,15 +5,19 @@ import {HiOutlineUser} from 'react-icons/hi'
 import {BsCardChecklist} from 'react-icons/bs'
 import {BsFolder} from 'react-icons/bs'
 import {TiMessages} from 'react-icons/ti'
+import { useState } from 'react'
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#')
+ 
   return (
     <nav>
-      <a href="#"><TbHome /></a>
-      <a href="#about"><HiOutlineUser /></a>
-      <a href="#experience"><BsCardChecklist /></a>
-      <a href="#portfolio"><BsFolder /></a>
-      <a href="#contact"><TiMessages /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><TbHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><HiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsCardChecklist /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsFolder /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TiMessages /></a>
     </nav>
   )
 }
