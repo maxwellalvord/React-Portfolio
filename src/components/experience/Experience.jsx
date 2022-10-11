2c6d2376a83ad05b965ccacffa8ac069fe98fa41
Maxwell Alvord <maxwellalvord@gmail.com>
2022-10-11 16:10:46 -0700

    add framework for experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import './experience.css'
+import {RiCheckboxCircleFill} from  'react-icons/ri'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <RiCheckboxCircleFill />
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <RiCheckboxCircleFill />
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <RiCheckboxCircleFill />
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <RiCheckboxCircleFill />
+              <h4>BootStrap</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <RiCheckboxCircleFill />
+              <h4>REACT</h4>
+              <small className='text-light'> Intermediate</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <RiCheckboxCircleFill />
+              <h4>Node.js</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <RiCheckboxCircleFill />
+              <h4>MySQL</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <RiCheckboxCircleFill />
+              <h4>FireBase</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <RiCheckboxCircleFill />
+              <h4>ASP MVC/.NET 5</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
