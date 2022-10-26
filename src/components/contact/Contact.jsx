99dbc36a4cc961007c0cd7be92491e84423fad81
Maxwell Alvord <maxwellalvord@gmail.com>
2022-10-26 15:02:30 -0700

    add styling to contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,26 +13,31 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            <AiOutlineMail/>
+            <AiOutlineMail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>maxwellalvord@gmail.com</h5>
-            <a href="mailto:maxwellalvord@gmail.com">Send a message</a>
+            <a href="mailto:maxwellalvord@gmail.com" target="_blank">Send a message</a>
           </article>
           <article className='contact__option'>
-            <AiOutlineIssuesClose/>
+            <AiOutlineIssuesClose className='contact__option-icon'/>
             <h4>Github issues page</h4>
             <h5>A Link!</h5>
-            <a href="https://github.com/maxwellalvord/React-Portfolio/issues">Send a message</a>
+            <a href="https://github.com/maxwellalvord/React-Portfolio/issues" target="_blank">Send a message</a>
           </article>
           <article className='contact__option'>
-            <BsFillTelephoneFill/>
+            <BsFillTelephoneFill className='contact__option-icon'/>
             <h4>Phone Number</h4>
             <h5>(503)-830-9753</h5>
             <a href="tel:5038309753">5038309753</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action=""></form>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name='message' rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
