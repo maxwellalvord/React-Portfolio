ac814f1873815c90d27511cc76a7790cff31e505
Maxwell Alvord <maxwellalvord@gmail.com>
2022-10-30 10:06:51 -0700

    complete contact component and implement emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,19 @@ import './contact.css'
 import {AiOutlineMail} from 'react-icons/ai'
 import {AiOutlineIssuesClose} from 'react-icons/ai'
 import {BsFillTelephoneFill} from 'react-icons/bs'
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_dtxl4zb', 'template_2tvx0um', form.current, '7CV7iHs4H4Sn8JO4G')
+
+    e.target.reset()
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -32,7 +43,7 @@ const Contact = () => {
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name='message' rows="7" placeholder='Your Message' required ></textarea>
